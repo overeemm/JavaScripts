014dfe4cd6b02a259babb569a6152cb511361964
Michiel Overeem <overeemm@gmail.com>
2011-12-15 18:58:05 +0000

    initial state painting works

--- overeemm.conway.js
+++ overeemm.conway.js
@@ -1,12 +1,19 @@
 var overeemm = window.overeemm || {};
+
+overeemm.log = function(msg) {
+     console.log(msg);
+};
+
 overeemm.conway = overeemm.conway || {};
 
-overeemm.conway.Life = function (canvas) {
+overeemm.conway.Life = function (canvas, args) {
+    
+    var blocksize = args.blocksize || 5;
     
     var elem = document.getElementById(canvas);
     var canvascontext = elem.getContext('2d');
-    var width = elem.getAttribute('width');
-    var height = elem.getAttribute('height');
+    var width = elem.getAttribute('width') / blocksize;
+    var height = elem.getAttribute('height') / blocksize;
     var state = {};
     
     var initState = function () {
@@ -24,24 +31,27 @@ overeemm.conway.Life = function (canvas) {
         initState();
         
         var pattern = elem.getAttribute('data-startpattern');
-        console.log('initial pattern '+pattern);
+        overeemm.log('initial pattern '+pattern);
         var startingpoints = pattern.split(';');
         for(var i = 0; i < startingpoints.length; i++){
             var x = startingpoints[i].split(',')[0];
             var y = startingpoints[i].split(',')[1];
-            console.log('init pixel ('+x+','+y+')');
+            overeemm.log('init pixel ('+x+','+y+')');
             state[x-1][y-1] = '1';
         }
     };
     
     var show = function () {
-        console.log('current state');
-        console.log(state);
+        overeemm.log('current state');
+        overeemm.log(state);
+        
+        canvascontext.fillStyle = "rgb(0,0,0)";
         for(var x = 0; x < width; x++) {
             for(var y = 0; y < height; y++) {
-                console.log(state[x][y] === '1');
-                canvascontext.fillStyle = (state[x][y] === '0') ? "rgb(0,0,0)" : "rgb(1,1,1)";
-                canvascontext.fillRect(x, y, 1, 1);
+                overeemm.log('('+x+','+y+') ' + (state[x][y] === '1' ? 'true' : 'false'));
+                
+                if(state[x][y] === '1')
+                    canvascontext.fillRect(x*blocksize, y*blocksize, blocksize, blocksize);
             }
         }
     };
