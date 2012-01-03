551ceaaaf124cc4bd0d779dfb6c326e2d09e3e35
Michiel Overeem <overeemm@gmail.com>
2012-01-03 18:53:34 +0000

    zooming working

--- overeemm.mandelbrot.js
+++ overeemm.mandelbrot.js
@@ -9,8 +9,18 @@ overeemm.mandelbrot = overeemm.mandelbrot || {};
 overeemm.mandelbrot.Number = function(options) {
     
     var repeatloops = 255;
-    var scale = options.scale;
+    var scale = 0.01;
     var colormodifier = options.colormodifier || function (r, g, b) { return { r: r, g: 255-g, b: 255-b}; };
+    
+    var setScale = function(sc) { 
+         var oldscale = scale;
+         if(typeof sc === 'function')
+             scale = sc(scale);
+         else
+             scale = sc; 
+             
+         return oldscale;
+    };
     
     var getColor = function(number) {
         
@@ -22,20 +32,23 @@ overeemm.mandelbrot.Number = function(options) {
         return colormodifier(r, g, b);
     };    
      
-    var getNumber = function (x, y) {
+    var getNumber = function (x, y, center) {
          
         var a = { start: 0.0, loop: 0.0, previous : 0.0 };
         var b = { start: 0.0, loop: 0.0, previous : 0.0 };
         var distance = 0.0;
         var number = 0;
 
+        x = center.x+(x*scale);
+        y = center.y+(y*scale);
+
         while (number < repeatloops && distance < 4) {
             a.previous = a.loop;
             b.previous = b.loop;
             a.loop = a.previous * a.previous - b.previous * b.previous + x;
             b.loop = 2 * a.previous * b.previous + y;
-            distance = Math.sqrt(  (Math.abs(a.start - a.loop) * Math.abs(a.start - a.loop)) 
-                                 + (Math.abs(b.start - b.loop) * Math.abs(b.start - b.loop))
+            distance = Math.sqrt( (Math.abs(a.start - a.loop) * Math.abs(a.start - a.loop)) +
+                                  (Math.abs(b.start - b.loop) * Math.abs(b.start - b.loop))
                                 );
             number++;
         }
@@ -44,13 +57,12 @@ overeemm.mandelbrot.Number = function(options) {
     
     return {
         getNumber : getNumber,
-        getColor : getColor
+        getColor : getColor,
+        setScale : setScale
     };
 };
 
 overeemm.mandelbrot.Painter = function(options) {
-    
-    var scale = options.scale || 0.01;
     
     var canvas = options.canvas;
     var canvascontext = canvas.getContext('2d');
@@ -58,12 +70,13 @@ overeemm.mandelbrot.Painter = function(options) {
     var canvasheight = parseInt(canvas.getAttribute('height'));
     
     var center = { x : 0.0, y : 0.0 };
-    var startpoint = { x : -1 * (canvaswidth / 2)
-                     , y : -1 * (canvasheight / 2) };
-    var endpoint = { x : startpoint.x + canvaswidth
-                   , y : startpoint.y + canvasheight };
+    var startpoint = { x : -1 * (canvaswidth / 2),
+                       y : -1 * (canvasheight / 2) };
+    var endpoint = { x : startpoint.x + canvaswidth,
+                     y : startpoint.y + canvasheight };
     
-    var number = overeemm.mandelbrot.Number({ scale : scale});
+    var number = overeemm.mandelbrot.Number({});
+    number.setScale(options.scale || 0.01);
     
     var setColor = function(rgb) {
          canvascontext.fillStyle = 'rgb(' + rgb.r + ',' + rgb.g + ',' + rgb.b + ')';
@@ -72,7 +85,7 @@ overeemm.mandelbrot.Painter = function(options) {
     var paint = function () {
         for (var screenX = startpoint.x; screenX < endpoint.x ; screenX++) {
             for (var screenY = startpoint.y; screenY < endpoint.y ; screenY++) {
-                var xynumber = number.getNumber( center.x+(screenX*scale), center.y+(screenY*scale) );
+                var xynumber = number.getNumber( screenX, screenY, center);
                 var color = number.getColor(xynumber);
                 setColor(color);
                 canvascontext.fillRect(screenX+200, screenY+200, 1, 1);
@@ -80,7 +93,33 @@ overeemm.mandelbrot.Painter = function(options) {
         }         
     };
     
-    return {
-         paint : paint
+    var getCoordinates = function(event, document, canvas) {
+        var x;
+        var y;
+        if (event.pageX || event.pageY) { 
+            x = event.pageX;
+            y = event.pageY;
+        } else { 
+            x = event.clientX + document.body.scrollLeft + document.documentElement.scrollLeft; 
+            y = event.clientY + document.body.scrollTop + document.documentElement.scrollTop; 
+        } 
+        x -= canvas.offsetLeft;
+        y -= canvas.offsetTop;
+        return { x : x, y : y };
     };
+    
+    var zoom = function(point) {
+        var oldscale = number.setScale( function(s) { return s/2; } );
+        
+        center.x = center.x + ( (point.x - (canvaswidth / 2)) * oldscale);
+	   center.y = center.y + ( (point.y - (canvasheight / 2)) * oldscale);
+    };
+    
+    var painter = {
+         paint : function () { paint(); return painter; },
+         setScale : function(scale) { number.setScale(scale); return painter; },
+         zoom : function (point) { zoom(point); return painter; },
+         getCoordinates : getCoordinates
+    };
+    return painter;
 };
